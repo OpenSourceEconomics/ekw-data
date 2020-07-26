b79f36c2610e96a7d8abd1070a9ddb025a68f4db
Rafael Suchy <suchy_rafael@icloud.com>
2020-07-26 03:17:17 +0200

    Added module with auxiliary functions

---
+++ career-decisions/aux_career_decisions_data.py
@@ -0,0 +1,10 @@
+from IPython.display import display_html
+
+
+def display_side_by_side(*args):
+    """Displays pd.DataFrames side by side in Jupyter notebooks"""
+
+    html_str = ""
+    for df in args:
+        html_str += df.to_html()
+    display_html(html_str.replace("table", 'table style="display:inline"'), raw=True)
