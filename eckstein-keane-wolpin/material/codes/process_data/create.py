1786b4f2c9cf55ddba41ed58aefd03368831153c
bekauf <benediktkauf1993@gmail.com>
2020-07-30 10:25:57 +0200

    ...and again

--- eckstein-keane-wolpin/material/codes/process_data/create.py
+++ eckstein-keane-wolpin/material/codes/process_data/create.py
@@ -2,7 +2,7 @@
 """
 """
 
-import preliminary_data_adjustments
+import preliminary_data_adjustments  # noqa: F401
 
-# import get_occ_codes_kw97
-import extended_kw97_dataclean
+# import get_occ_codes_kw97  # noqa: F401
+import extended_kw97_dataclean  # noqa: F401
